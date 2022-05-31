6b75d2e27358339b1687098f83d16e9c8e0a1798
tolmdyn <tolm.dyn@gmail.com>
2022-05-31 16:23:18 +0100

    part 4.1 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,43 +1,25 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+//const cors = require('cors')
 
-const Blog = mongoose.model('Blog', blogSchema)
+//const mongoose = require('mongoose')
 
-const mongoUrl = 'mongodb+srv://tolmdyn:xVPaxH72Xj9lbSzX@cluster0.p5lkg.mongodb.net/blognotes?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+//const mongoUrl = 'mongodb+srv://tolmdyn:xVPaxH72Xj9lbSzX@cluster0.p5lkg.mongodb.net/blognotes?retryWrites=true&w=majority'
+//mongoose.connect(mongoUrl)
 
-app.use(cors())
-app.use(express.json())
+//a//pp.use(cors())
+//app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+//const PORT = 3003
+//app.listen(PORT, () => {
+//  console.log(`Server running on port ${PORT}`)
+//})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+const server = http.createServer(app)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
